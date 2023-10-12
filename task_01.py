fbf57c801f41aaf515a914a1cd0e27c6bf7c304b
Saad Nisar Butt <sb03750@gmail.com>
2023-10-12 12:37:29 +0500

    functions and modules

--- task_01.py
+++ task_01.py
@@ -1,50 +1,9 @@
-# # print('I told my friend, "Python is my favorite language!"')
-# # print("The language 'Python' is named after Monty Python, not the snake.")
-# # print("One of Python's strengths is its diverse and supportive community.")
-# name = "ada lovelace"
-# # print(name.title())
-# # print(name.upper())
-# # print(name.lower())
-# first_name = "ada"
-# last_name = "lovelace"
-# full_name = f"{first_name} {last_name}"
-# # print(full_name)
-# # print(f"Hello {full_name.title()}, would you like to learn some Python today?")
-# # print("\tPython\nSaad")
-# nostarch_url = 'https://nostarch.com'
-# # print(nostarch_url.removeprefix('https://'))
-# # print(nostarch_url)
-# famous_person = "Albert Einstein"
-# quote = f'\t{famous_person} once said, "A person who never made a mistake never \n\ttried anything new."'
-# # print(quote)
 
-# persons_name = "   \tada \nlovelace  "
-# # print(persons_name.strip())
-
-# file_name = 'python_notes.txt'
-# file_name.removesuffix("python_notes.txt")
-# # print(file_name.removesuffix(".txt"))
-
-# # print("saad"+file_name)
-
-# universe_age = 14_000_000_000
-# # print(universe_age)
-
-# x,y,z = 0,"saad",3
-# # print(x,y,z)
-
-# MAX_CONNECTIONS = 5000
-# # print(MAX_CONNECTIONS)
-# MAX_CONNECTIONS = 600
-# # print(MAX_CONNECTIONS)
-
-# # import this
-# bicycles = ['trek', 'cannondale', 'redline', 'specialized']
-# print(bicycles)
-
-# numbers = list(range(1, 11))
-# print(sum(numbers))
-
-message = input("Tell me something, I'll repeat it back to you.\n")
-print(message)
-
+def make_pizza(size, *toppings):
+    """Summarize the pizza we are about to make."""
+    print(f"\nMaking a {size}-inch pizza with the following toppings:")
+    for topping in toppings:
+        print(f"- {topping}")
+        
+def add(a, b):
+    return a + b
