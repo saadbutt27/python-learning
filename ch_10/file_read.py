b10aaad6ebe1906b940c57de206c2ffb0f683a9d
Saad Nisar Butt <sb03750@gmail.com>
2023-10-20 16:46:00 +0500

    installed mypy and updates

--- ch_10/file_read.py
+++ ch_10/file_read.py
@@ -1,16 +1,16 @@
 from pathlib import Path 
 
-path = Path("ch_10/text_files/pi_file.txt")
-path = Path("ch_10/text_files/pi_million.txt")
-contents = path.read_text()
+# path = Path("ch_10/text_files/pi_file.txt")
+# path = Path("ch_10/text_files/pi_million.txt")
+# contents = path.read_text()
 # print(contents)
 
-lines = contents.splitlines() # splitlines function will spit the lines and make a list
+# lines = contents.splitlines() # splitlines function will spit the lines and make a list
 # print(lines)
 
-pi_string = ""
-for line in lines:
-    pi_string += line.lstrip()
+# pi_string = ""
+# for line in lines:
+#     pi_string += line.lstrip()
 
 # print(pi_string[:40])
 
@@ -22,8 +22,7 @@ for line in lines:
 # else: 
 #     print("Nuhuuu")
 
-
-path = Path("ch_10/text_files/alice.txt")
+path = Path("text_files/alice.txt")
 try:
     contents = path.read_text()
 except FileNotFoundError:
@@ -36,5 +35,5 @@ else:
     # for line in contents.splitlines():
     #     line = line.replace("python", "javascript")
     #     print(line)
-    contents = contents.split()
-    print(contents, len(contents))
+    contents_1 = contents.split()
+    print(contents_1, len(contents_1))
